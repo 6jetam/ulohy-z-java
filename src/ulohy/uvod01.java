a41a1ab36e4bf1bbf19347b29b292469ef4f5a77
6jetam <1matejsoltes1@gmail.com>
2023-09-03 12:02:05 +0200

    program na scitanie 2 cisel

--- src/ulohy/uvod01.java
+++ src/ulohy/uvod01.java
@@ -1,1 +1,18 @@
+package ulohy;
 //uvod do java
+import java.util.Scanner;
+
+public class uvod01 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.println("Enter your buying price per share:");
+		double num1 = scan.nextDouble();
+		double num2 = scan.nextDouble();
+		int num3 = (int) (num1 + num2);
+		
+		System.out.println("Sucet 2 cisel:" + num3);
+	
+	}
+}
